4db84ebbd4c15e351fc142b6bfc9b7d6263b0625
Pedro Soares <pmbs.123@gmail.com>
2026-05-17 13:53:19 +0200

    feat: support BF version 2025.12

--- src/headers.rs
+++ src/headers.rs
@@ -443,6 +443,7 @@ pub(crate) enum InternalFirmware {
     Betaflight4_3,
     Betaflight4_4,
     Betaflight4_5,
+    Betaflight2025_12,
     Inav5,
     Inav6,
     Inav7,
@@ -455,6 +456,7 @@ impl InternalFirmware {
             Self::Betaflight4_2
             | Self::Betaflight4_3
             | Self::Betaflight4_4
+              Self::Betaflight2025_12
             | Self::Betaflight4_5 => true,
             Self::Inav5 | Self::Inav6 | Self::Inav7 | Self::Inav8 => false,
         }
@@ -483,6 +485,9 @@ impl From<Firmware> for InternalFirmware {
             Firmware::Betaflight(FirmwareVersion {
                 major: 4, minor: 5, ..
             }) => Self::Betaflight4_5,
+            Firmware::Betaflight(FirmwareVersion {
+                major: 2025, minor: 12, ..
+            }) => Self::Betaflight2025_12,
             Firmware::Inav(FirmwareVersion { major: 5, .. }) => Self::Inav5,
             Firmware::Inav(FirmwareVersion { major: 6, .. }) => Self::Inav6,
             Firmware::Inav(FirmwareVersion { major: 7, .. }) => Self::Inav7,
